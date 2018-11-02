7e4910e00b32b634b094073039335f2ccf98e343
Bnl <bnl@mmmm.mm>
2018-11-02 14:27:37 +0300

    Spring Security

---
+++ src/main/java/conference/config/SecurityWebInitializer.java
@@ -0,0 +1,9 @@
+package conference.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Spring Security web initializer.
+ */
+public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
+}
