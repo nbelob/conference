67c3cb6f202ccc7f45cf6208aa2a2aef3b48d9ca
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-12-09 19:19:06 +0300

    Add tests

---
+++ src/test/java/conference/dao/AccountDaoTest.java
@@ -0,0 +1,33 @@
+package conference.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Account DAO test.
+ */
+public class AccountDaoTest {
+    @Test
+    public void addTest() {
+        //TODO: implement
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void updateTest() {
+        //TODO: implement
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void deleteTest() {
+        //TODO: implement
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void findByUsernameTest() {
+        //TODO: implement
+        Assert.assertTrue(true);
+    }
+}
