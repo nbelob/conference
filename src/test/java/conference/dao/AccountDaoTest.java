0ffde90dbccbaecba92aba1c8a25ea22a227ec47
bnl <b_n_l@mail.ru>
2018-12-15 22:40:34 +0300

    test

--- src/test/java/conference/dao/AccountDaoTest.java
+++ src/test/java/conference/dao/AccountDaoTest.java
@@ -1,5 +1,7 @@
 package conference.dao;
 
+import conference.dao.exception.AccountNotExistsException;
+import conference.domain.Account;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,27 +9,34 @@ import org.junit.Test;
  * Account DAO test.
  */
 public class AccountDaoTest {
+    private static AccountDao accountDao;
+
     @Test
-    public void addTest() {
-        //TODO: implement
-        Assert.assertTrue(true);
+    public void addTest() throws AccountNotExistsException {
+        accountDao.add("bnl", "qwerty");
+        Account account = accountDao.findByUsername("bnl");
+        Assert.assertEquals("bnl", account.getUserName());
+        Assert.assertEquals("qwerty", account.getPassword());
     }
 
     @Test
-    public void updateTest() {
-        //TODO: implement
-        Assert.assertTrue(true);
+    public void updateTest() throws AccountNotExistsException {
+        accountDao.update("jsmith", "passwnew");
+        Account account = accountDao.findByUsername("jsmith");
+        Assert.assertEquals("jsmith", account.getUserName());
+        Assert.assertEquals("passwnew", account.getPassword());
+    }
+
+    @Test(expected = AccountNotExistsException.class)
+    public void deleteTest() throws AccountNotExistsException {
+        accountDao.delete("jdoe");
+        accountDao.findByUsername("jdoe");
     }
 
     @Test
-    public void deleteTest() {
-        //TODO: implement
-        Assert.assertTrue(true);
-    }
-
-    @Test
-    public void findByUsernameTest() {
-        //TODO: implement
-        Assert.assertTrue(true);
+    public void findByUsernameTest() throws AccountNotExistsException {
+        Account account = accountDao.findByUsername("rroe");
+        Assert.assertEquals("rroe", account.getUserName());
+        Assert.assertEquals("password", account.getPassword());
     }
 }
--- src/test/java/conference/dao/MessageDaoTest.java
+++ src/test/java/conference/dao/MessageDaoTest.java
@@ -7,15 +7,19 @@ import org.junit.Test;
  * Message DAO test.
  */
 public class MessageDaoTest {
+    private static MessageDao messageDao;
+
     @Test
     public void addTest() {
-        //TODO: implement
-        Assert.assertTrue(true);
+        messageDao.add("jsmith", "Mess");
+        Assert.assertEquals("jsmith", messageDao.findAll().get(messageDao.findAll().size() - 1).getUserName());
+        Assert.assertEquals("Mess", messageDao.findAll().get(messageDao.findAll().size() - 1).getText());
     }
 
     @Test
     public void findAllTest() {
-        //TODO: implement
-        Assert.assertTrue(true);
+        int i = messageDao.findAll().size();
+        messageDao.add("jsmith", "Mess1");
+        Assert.assertEquals(i + 1, messageDao.findAll().size());
     }
 }
