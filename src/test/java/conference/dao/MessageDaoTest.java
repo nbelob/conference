be9278f996374583ff3580d2e3affffccf1a34ae
bnl <b_n_l@mail.ru>
2018-12-17 23:14:58 +0300

    test

--- src/test/java/conference/dao/MessageDaoTest.java
+++ src/test/java/conference/dao/MessageDaoTest.java
@@ -1,11 +1,14 @@
 package conference.dao;
 
+import conference.domain.Message;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import java.util.List;
 
 /**
  * Message DAO test.
@@ -25,15 +28,28 @@ public class MessageDaoTest {
 
     @Test
     public void addTest() {
-        messageDao.add("jsmith", "Mess");
+/*        messageDao.add("jsmith", "Mess");
         Assert.assertEquals("jsmith", messageDao.findAll().get(0).getUserName());
-        Assert.assertEquals("Mess", messageDao.findAll().get(0).getText());
+        Assert.assertEquals("Mess", messageDao.findAll().get(0).getText());*/
+        messageDao.add("jsmith", "Mess");
+        List<Message> messages = messageDao.findAll();
+        Assert.assertEquals(4, messages.size());
+        Assert.assertEquals("jsmith",
+                messages.get(0).getUserName());
+        Assert.assertEquals("Mess",
+                messages.get(0).getText());
     }
 
     @Test
     public void findAllTest() {
-        int i = messageDao.findAll().size();
+/*        int i = messageDao.findAll().size();
         messageDao.add("jsmith", "Mess1");
-        Assert.assertEquals(i + 1, messageDao.findAll().size());
-    }
+        Assert.assertEquals(i + 1, messageDao.findAll().size());*/
+        List<Message> messages = messageDao.findAll();
+
+        Assert.assertEquals(3, messages.size());
+        Assert.assertEquals("jsmith",
+                messages.get(0).getUserName());
+        Assert.assertEquals("Some people care too much. I think it''s called love.",
+                messages.get(0).getText());    }
 }
